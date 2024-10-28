aaf733292b0153f734bd721174515c5680f5f048
vannhan-1602 <vovannhan160204@gmail.com>
2024-10-28 23:42:03 +0700

    init project

---
+++ mycode/src/mycode/Mycode.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mycode;
+
+/**
+ *
+ * @author VanNhan
+ */
+public class Mycode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("HEllo world");
+    }
+    
+}
